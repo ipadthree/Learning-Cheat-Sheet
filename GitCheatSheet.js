9600999126dc2ec44474e1ad093ab7fab82bb073
jliang <jialian@microsoft.com>
2017-01-04 11:04:24 -0800

    1/4/17

---
+++ GitCheatSheet.js
@@ -0,0 +1,14 @@
+Rename a local and remote branch in git
+
+1. Rename your local branch.
+If you are on the branch you want to rename:
+git branch -m new-name
+If you are on a different branch:
+git branch -m old-name new-name
+
+2. Delete the old-name remote branch and push the new-name local branch.
+git push origin :old-name new-name
+
+3. Reset the upstream branch for the new-name local branch.
+Switch to the branch and then:
+git push origin -u new-name
