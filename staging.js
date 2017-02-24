3ea97648e12332535b9e7256dea71201c90184c8
jliang <jialian@microsoft.com>
2017-02-24 11:46:12 -0800

    staging

---
+++ staging.js
@@ -0,0 +1,5 @@
+在staing里 inspect会发现在 <head>里有一个这个tag，content表示这个是哪个 SHA 哪个commit。
+<meta name="yamjs-revision" content="fe7514eb457c4d55054e1182b0c6b6e53acf709a">
+
+然后copy 这个 SHA, 在github的 url的后面               在这里改上这个 sha 就能跳到这是哪个commit了
+https://github.int.yammer.com/yammer/yamjs/commit/fe7514eb457c4d55054e1182b0c6b6e53acf709a
