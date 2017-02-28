67135cb060bd01c3d0aeacfebc75ecd6beb2d8e9
jliang <jialian@microsoft.com>
2017-02-28 11:44:07 -0800

    yammer

---
+++ Yammer工作/Charles_for_Firefox.js
@@ -0,0 +1,7 @@
+1、先在Charles > help > SSL Proxying 里点击 Save Charles Root Certificate 把
+    Charles Certificate 存到电脑里的任意地方。
+
+2、然后在 Firefox > Preference > Advanced > Certificates 里点击 View Certificates
+    再点击 Import, 把刚刚存到电脑里的Charles Certificate import 进来
+
+3、restart firefox 就配置好了
